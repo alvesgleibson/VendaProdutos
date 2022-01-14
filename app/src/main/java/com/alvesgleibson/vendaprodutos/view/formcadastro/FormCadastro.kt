a4478ebf8e87fdf5e6b141b4b460560862488191
Gleibson Alves <alvesgleibson@gmail.com>
2022-01-14 00:18:20 -0300

    Layout cadastro finalizado

---
+++ app/src/main/java/com/alvesgleibson/vendaprodutos/view/formcadastro/FormCadastro.kt
@@ -0,0 +1,12 @@
+package com.alvesgleibson.vendaprodutos.view.formcadastro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alvesgleibson.vendaprodutos.R
+
+class FormCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_cadastro)
+    }
+}
